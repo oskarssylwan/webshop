4c542a6cb5b3fc13985913f7c2c1f83c0435de3b
Oskars Sylwan <oskars.sylwan@gmail.com>
2020-10-26 17:34:10 +0100

    User authorizaition middleware now uses secret from config

--- packages/api/src/middleware.js
+++ packages/api/src/middleware.js
@@ -1,10 +1,11 @@
 const webToken = require('jsonwebtoken');
+const config = require('./config')
 
 function protected(req, res, next) {
   const token = req.body.token || req.query.token || req.headers['x-access-token'];
 
   if (token) {
-    webToken.verify(token, 'morello', (error, decoded) => {
+    webToken.verify(token, config.token_secret, (error, decoded) => {
       if (error)  {
          return next(error);
       } else {
